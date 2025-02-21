95fb602e7e7a6cea564fc5d9e3d2393732ad5a50
Rejas181 <167266035+Rejas181@users.noreply.github.com>
2025-02-21 12:37:30 -0600

    Update mode_GS.cpp

--- ArduPlane/mode_GS.cpp
+++ ArduPlane/mode_GS.cpp
@@ -13,8 +13,12 @@ void ModeGS::update()
     if (AP::ahrs().airspeed_vector_true(airspeed_vec_bf)) {
         // if we don't have an airspeed estimate then we don't have a
         // valid wind estimate on blimps
-        printf("velocidad aerodinamica esitmada: %f, %f, %f,\n",airspeed_vec_bf[0],airspeed_vec_bf[1],airspeed_vec_bf[2]);
+        //printf("velocidad aerodinamica esitmada: %f, %f, %f,\n",airspeed_vec_bf[0],airspeed_vec_bf[1],airspeed_vec_bf[2]);
+        printf("velocidad aerodinamica esitmada: %f, %f, %f,\n",airspeed_vec_bf.x,airspeed_vec_bf.y,airspeed_vec_bf.z);
     }
+
+    //_ang_vel_body.x = _ahrs.get_gyro().x;
+    //_ang_vel_body.z = _ahrs.get_gyro().z;
     
    
 }
