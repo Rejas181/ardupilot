930d83f2bc6d86cbd175dcd0cbc697e57fe502de
Rejas181 <167266035+Rejas181@users.noreply.github.com>
2025-02-23 17:48:06 -0600

    Update mode_GS.cpp

--- ArduPlane/mode_GS.cpp
+++ ArduPlane/mode_GS.cpp
@@ -64,10 +64,7 @@ void ModeGS::update()
     
     //------------------------------------Salidas PWM-------------------------------------
     //Envio de señales PWM
-    SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, plane.roll_in_expo(false));         //Salida manual del alerón (obtenida de: mode manual)
-    SRV_Channels::set_output_pwm_chan(SRV_Channel::k_elevator, pitch_out);                      //Salida del elevador set_output_pwm_chan
-    output_rudder_and_steering(plane.rudder_in_expo(false));                                    //Salida manual del Timón (obtenida de: mode manual)
-    SRV_Channels::set_output_pwm_chan(SRV_Channel::k_throttle, throttle_out);                   //Salida del acelerador
+    
    
    
 }
@@ -78,5 +75,10 @@ void ModeGS::run()
     //Mode::run();
     reset_controllers();
     Control.initialize();                    // Initialize model
+
+    SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, plane.roll_in_expo(false));         //Salida manual del alerón (obtenida de: mode manual)
+    SRV_Channels::set_output_pwm_chan(SRV_Channel::k_elevator, pitch_out);                      //Salida del elevador set_output_pwm_chan
+    output_rudder_and_steering(plane.rudder_in_expo(false));                                    //Salida manual del Timón (obtenida de: mode manual)
+    SRV_Channels::set_output_pwm_chan(SRV_Channel::k_throttle, throttle_out);                   //Salida del acelerador
     //output_pilot_throttle();
 }
