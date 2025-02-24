91e704c4b0741c2377eefb54087dbc934b8c9dd6
Rejas181 <167266035+Rejas181@users.noreply.github.com>
2025-02-23 19:17:59 -0600

    lastia puta eran 4500

--- ArduPlane/mode_manual.cpp
+++ ArduPlane/mode_manual.cpp
@@ -5,7 +5,7 @@ void ModeManual::update()
 {
     SRV_Channels::set_output_scaled(SRV_Channel::k_aileron, plane.roll_in_expo(false));
     SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, plane.pitch_in_expo(false));
-    printf("valor en expo: %f\n", plane.pitch_in_expo(false));
+    //printf("valor en expo: %f\n", plane.pitch_in_expo(false));
     output_rudder_and_steering(plane.rudder_in_expo(false));
 
     const float throttle = plane.get_throttle_input(true);
