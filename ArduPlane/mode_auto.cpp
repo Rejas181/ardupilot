c1b32089cf7726f9f039f8161a615421b3db21b2
Rejas181 <167266035+Rejas181@users.noreply.github.com>
2025-07-20 12:49:58 -0600

    Update mode_auto.cpp

--- ArduPlane/mode_auto.cpp
+++ ArduPlane/mode_auto.cpp
@@ -62,6 +62,34 @@ void ModeAuto::_exit()
 
 void ModeAuto::update()
 {
+
+    // ===================================== datos a registrar ==================================================================================
+    float pitch_input = plane.channel_pitch->norm_input();       //demanda de cabeceo normalizado de -1 a 1 de 1000 a 2000 PWM
+    float throttle_input = plane.channel_throttle->norm_input(); //demanda de acelerador  normalizado de -1 a 1 de 1000 a 2000 PWM
+    float gamma_d;                                               //valor de gamma deseado   
+
+    if(pitch_input > 0) {gamma_d=pitch_input*16;}                 //ajuste del angulo deseado de cabeceo
+    else {gamma_d=pitch_input*24;}
+    float V_d = 26*throttle_input+10;
+
+    //float temp = pitch_input+throttle_input;            //Variable temporal para que no me marque error el compilado
+    //printf("variable temporal: %f \n",temp);            //Función temporal para que no me marque error el compilado 
+   
+    //--------------------Datos de los sensores--------------------
+    Vector3f airspeed_vec_bf;                           //Vector de Velocidad aerodinámica en sistema cuerpo en m/s
+    Vector3f vel_ang;                                   //Vector de velocidad angular en sistema cuerpo en lo que yo espero sean rad/s
+    
+    // display -- el if es para verificar que si hay un dato para mostrar
+    AP::ahrs().airspeed_vector_true(airspeed_vec_bf);
+    
+    //velocidad angular
+    vel_ang = ahrs.get_gyro();
+    float theta=ahrs.get_pitch();
+
+    plane.Log_Write_Manual(float(V_d),float(gamma_d),float(airspeed_vec_bf.x),float(airspeed_vec_bf.z),float(theta),float(vel_ang.y)); //Escritura del registro
+
+    //======================================Control original=========================================================================
+
     if (plane.mission.state() != AP_Mission::MISSION_RUNNING) {
         // this could happen if AP_Landing::restart_landing_sequence() returns false which would only happen if:
         // restart_landing_sequence() is called when not executing a NAV_LAND or there is no previous nav point
